7226d05703b8773f43454689449e6d252587a710
saminulh <saminulh@hotmail.com>
2017-09-23 18:39:11 -0400

    difficulty

--- src/General/Difficulty.java
+++ src/General/Difficulty.java
@@ -1,13 +1,35 @@
 package General;
 
+import com.orbischallenge.firefly.client.objects.models.EnemyUnit;
+import com.orbischallenge.firefly.client.objects.models.FriendlyUnit;
+import com.orbischallenge.firefly.client.objects.models.World;
+import com.orbischallenge.firefly.client.objects.models.Tile;
+
+import com.orbischallenge.game.engine.Point;
+import com.orbischallenge.firefly.objects.enums.Direction;
+
+import com.orbischallenge.logging.Log;
+
+import java.util.*;
+
 public class Difficulty implements Comparable<Difficulty>{
     private boolean possible;
+    private int nearbyWalls, nearbyFriendly, wallsNearAdjacent;
+
+    public Difficulty(Tile t, World world){
+        if (!t.isNeutral()){
+            possible = false;
+        }
+        else{
+            for (Tile a: world.getTilesAround(t.getPosition()).values()){
+
+            }
+        }
+
+    }
 
     @Override
     public int compareTo(Difficulty o) {
-        if (o == WORST_DIFFICULTY){
-            return 1;
-        }
         return 0;
     }
 }
