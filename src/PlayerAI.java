80e9e0d709710552cd3c72ce9c4f21530ef5aba1
saminulh <saminulh@hotmail.com>
2017-09-23 15:11:51 -0400

    outline

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -16,25 +16,32 @@ public class PlayerAI {
     public PlayerAI() {
         // Any instantiation code goes here
     }
+    int turns = 0;
+
 
     /**
      * This method will get called every turn.
      *
-     * @param world         The latest state of the world.
+     * @param world The latest state of the world.
      * @param friendlyUnits An array containing all remaining firefly units in your team
-     * @param enemyUnits    An array containing all remaining enemy firefly units
+     * @param enemyUnits An array containing all remaining enemy firefly units
+     *
      */
+
     public void doMove(World world, FriendlyUnit[] friendlyUnits, EnemyUnit[] enemyUnits) {
         /* Fly away to freedom, daring fireflies
         Build thou nests
         Grow, become stronger
         Take over the world */
-        for (FriendlyUnit unit : friendlyUnits) {
+        /*
+        for (FriendlyUnit unit: friendlyUnits) {
             List<Point> path = world.getShortestPath(unit.getPosition(),
-                    world.getClosestCapturableTileFrom(unit.getPosition(), null).getPosition(),
-                    null);
+                                                     world.getClosestCapturableTileFrom(unit.getPosition(), null).getPosition(),
+                                                     null);
             if (path != null) world.move(unit, path.get(0));
-            System.out.println();
-        }
+        }*/
+        turns += 1;
+
+
     }
 }
---
+++ src/Squad.java
@@ -0,0 +1,8 @@
+import MissionTypes.Mission;
+import com.orbischallenge.firefly.client.objects.models.FriendlyUnit;
+
+
+public class Squad {
+    FriendlyUnit[] workers;
+    Mission mission;
+}
