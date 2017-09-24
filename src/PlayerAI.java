7becedf1f062efefc3790ff8d2f67ca43b3fa890
ArthurRab <ArthurRabinovich1@gmail.com>
2017-09-24 01:01:39 -0400

    Changed to unitID thingy to arraylist
    Made it so that missions of merged dudes get deleted. Hopefully

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -2,7 +2,6 @@ import com.orbischallenge.firefly.client.objects.models.EnemyUnit;
 import com.orbischallenge.firefly.client.objects.models.FriendlyUnit;
 import com.orbischallenge.firefly.client.objects.models.World;
 import com.orbischallenge.game.engine.Point;
-import org.monte.media.Player;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,7 +11,7 @@ public class PlayerAI {
     // Any field declarations go here
 
     public static World world;
-    public static UnitWrapper[] friendlyUnits;
+    public static ArrayList<UnitWrapper> friendlyUnits;
     public static Collection<Point> AVOID_AT_ALL_COSTS = new ArrayList<>();
 
     HashMap<String, UnitWrapper> unitIDToWrapper = new HashMap<String, UnitWrapper>();
@@ -40,8 +39,10 @@ public class PlayerAI {
     public void doMove(World world, FriendlyUnit[] friendlyUnits, EnemyUnit[] enemyUnits) {
         PlayerAI.world = world;
         MissionManager.getInstance().update();
-        UnitWrapper[] fUnits = new UnitWrapper[friendlyUnits.length];
-        for (int i=0; i<friendlyUnits.length;i++) {
+
+
+        PlayerAI.friendlyUnits.clear();
+        for (int i = 0; i < friendlyUnits.length; i++) {
             FriendlyUnit f = friendlyUnits[i];
             UnitWrapper w = unitIDToWrapper.getOrDefault(f.getUuid(), null);
             if (w == null) {
@@ -49,10 +50,16 @@ public class PlayerAI {
                 unitIDToWrapper.put(f.getUuid(), w);
             }
 
-            fUnits[i] = w;
+            PlayerAI.friendlyUnits.add(w);
         }
 
-        PlayerAI.friendlyUnits = fUnits;
+
+        for (String i : unitIDToWrapper.keySet()) {
+            if (!PlayerAI.friendlyUnits.contains(unitIDToWrapper.get(i))) {
+                unitIDToWrapper.get(i).deleteMission();
+                unitIDToWrapper.remove(i);
+            }
+        }
         if (turns == 0) {
             nests = nlf.findNestLocations(PlayerAI.world, world.getFriendlyNestPositions()[0], 4);
             for (Point p : nlf.getNeighbours(world, nests)) {
@@ -64,8 +71,8 @@ public class PlayerAI {
         System.out.println(MissionManager.getInstance().pendingMissions.size());
         System.out.println(MissionManager.getInstance().saveForLater.size());
 
-        System.out.println(PlayerAI.friendlyUnits.length);
-        for (UnitWrapper i:PlayerAI.friendlyUnits) {
+        System.out.println(PlayerAI.friendlyUnits.size());
+        for (UnitWrapper i : PlayerAI.friendlyUnits) {
             i.update();
         }
 
